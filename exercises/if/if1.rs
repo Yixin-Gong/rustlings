db18ea561ac1a7423d992c0d1020339e2c798afb
eric <3258859094@qq.com>
2023-06-27 03:07:59 +0800

    Implement if and quiz1

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -2,15 +2,20 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
     // - additional variables
+    if a>b {a}
+    else  {b}
 }
 
+fn main(){
+    bigger(1,2);
+}
 // Don't mind this for now :)
 #[cfg(test)]
 mod tests {
